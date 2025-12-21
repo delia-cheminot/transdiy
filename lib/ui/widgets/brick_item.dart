e7509499ff7c0c690ca708bc861d098b6ffff9e3
Alice Lorido <lorido.alice@gmail.com>
2025-12-21 19:37:38 +0100

    format

--- lib/ui/widgets/brick_item.dart
+++ lib/ui/widgets/brick_item.dart
@@ -7,44 +7,42 @@ import 'package:transdiy/data/providers/medication_schedule_provider.dart';
 class BrickTile extends StatelessWidget {
   const BrickTile({super.key});
 
-
   @override
-   Widget build(BuildContext context) {
-    final medicationScheduleProvider = context.watch<MedicationScheduleProvider>();
+  Widget build(BuildContext context) {
+    final medicationScheduleProvider =
+        context.watch<MedicationScheduleProvider>();
     return Scaffold(
       appBar: AppBar(title: const Text('à venir')),
       body: ListView(
-          children : <Widget>[
-            for (final schedule in medicationScheduleProvider.schedules)
-              _buildTile(schedule),
-          
+        children: <Widget>[
+          for (final schedule in medicationScheduleProvider.schedules)
+            _buildTile(schedule),
           Divider(height: 0),
         ],
       ),
-     );
-   }
+    );
+  }
 
-   String _getPharmacyAsset(MedicationSchedule schedule){
+  String _getPharmacyAsset(MedicationSchedule schedule) {
     return "assets/pharmacie/tablets/full_tablet.svg";
-   }
+  }
 
-   String _getTreatmentName(MedicationSchedule schedule){
+  String _getTreatmentName(MedicationSchedule schedule) {
     return "Oestradiol Enanthate";
-   }
+  }
 
-   String _getTreatmentDate(MedicationSchedule schedule){
+  String _getTreatmentDate(MedicationSchedule schedule) {
     return "fuck that shit bro";
-   }
+  }
 
-   ListTile _buildTile(MedicationSchedule schedule) {
+  ListTile _buildTile(MedicationSchedule schedule) {
     return ListTile(
-            leading: CircleAvatar(
-                backgroundColor : Colors.transparent,
-                child: SvgPicture.asset(_getPharmacyAsset(schedule))),
-            title: Text(_getTreatmentName(schedule)),
-            subtitle: Text(_getTreatmentDate(schedule)),
-            trailing: Icon(Icons.favorite_rounded),
-            );
-        
-   }
+      leading: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          child: SvgPicture.asset(_getPharmacyAsset(schedule))),
+      title: Text(_getTreatmentName(schedule)),
+      subtitle: Text(_getTreatmentDate(schedule)),
+      trailing: Icon(Icons.favorite_rounded),
+    );
+  }
 }
