0ce88ed83ac607514a6bf698f0dc458b84481daf
délia <delia.cheminot@gmail.com>
2025-12-27 17:47:33 +0100

    clean up homepage

--- lib/ui/views/home/home_page.dart
+++ lib/ui/views/home/home_page.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:transdiy/ui/widgets/main_page_schedule.dart';
 
-
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BrickTile().build(context);
+    return MainPageSchedule().build(context);
   }
 }
