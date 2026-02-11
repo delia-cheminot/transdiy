5aaacb74d196de10f77ea97f023f87b054ecc2f5
Délia Cheminot <delia@cheminot.net>
2026-02-11 17:16:38 +0100

    remove test button from homepage

--- lib/ui/views/home/home_page.dart
+++ lib/ui/views/home/home_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
 import 'package:mona/data/model/medication_schedule.dart';
 import 'package:mona/data/providers/medication_schedule_provider.dart';
-import 'package:mona/services/notification_service.dart';
 import 'package:mona/ui/views/home/take_medication_page.dart';
 import 'package:mona/ui/widgets/main_page_wrapper.dart';
 import 'package:provider/provider.dart';
@@ -19,19 +18,6 @@ class HomePage extends StatelessWidget {
           emptyMessage: 'Add a schedule in your profile to get started!',
           child: SingleChildScrollView(
             child: Column(children: [
-              ElevatedButton(
-                onPressed: () => NotificationService().scheduleNotification(
-                  id: 0,
-                  title: 'Test Notification',
-                  body: 'This is a test notification body.',
-                  year: DateTime.now().year,
-                  month: DateTime.now().month,
-                  day: DateTime.now().day,
-                  hour: DateTime.now().hour,
-                  minute: DateTime.now().minute + 1,
-                ),
-                child: Text('schedule notification'),
-              ),
               for (final schedule in medicationScheduleProvider.schedules)
                 _buildTile(schedule, context),
             ]),
