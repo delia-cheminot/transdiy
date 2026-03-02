9f304a10da4cdb89020e162a555b59633d5fb4e1
délia <delia@cheminot.net>
2026-03-02 15:55:45 +0100

    add daysBetweenDate helper

--- lib/util/date_helpers.dart
+++ lib/util/date_helpers.dart
@@ -6,3 +6,8 @@ DateTime normalizedToday() {
   final now = DateTime.now();
   return normalizeDate(now);
 }
+
+int daysBetweenDate(DateTime date, {DateTime? origin}) {
+  final reference = origin ?? DateTime.now();
+  return normalizeDate(date).difference(normalizeDate(reference)).inDays.abs();
+}
