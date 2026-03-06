36e4975dfc18bacf312878cbf5ba955a2e564b67
délia <delia@cheminot.net>
2026-03-06 18:17:53 +0100

    add molecule/route/ester to new item page

--- lib/ui/views/supplies/new_item_page.dart
+++ lib/ui/views/supplies/new_item_page.dart
@@ -3,6 +3,9 @@ import 'package:mona/data/model/administration_route.dart';
 import 'package:mona/data/model/molecule.dart';
 import 'package:mona/data/model/supply_item.dart';
 import 'package:mona/data/providers/supply_item_provider.dart';
+import 'package:mona/services/preferences_service.dart';
+import 'package:mona/ui/widgets/forms/form_dropdown_field.dart';
+import 'package:mona/ui/widgets/forms/form_spacer.dart';
 import 'package:mona/ui/widgets/forms/form_text_field.dart';
 import 'package:mona/ui/widgets/forms/model_form.dart';
 import 'package:mona/util/decimal_helpers.dart';
@@ -17,32 +20,67 @@ class _NewItemPageState extends State<NewItemPage> {
   late TextEditingController _totalDoseController;
   late TextEditingController _nameController;
   late TextEditingController _concentrationController;
+  Molecule? _molecule;
+  AdministrationRoute? _administrationRoute;
+  Ester? _ester;
+  late PreferencesService _preferencesService;
 
   String? get _nameError => SupplyItem.validateName(_nameController.text);
   String? get _totalDoseError =>
       SupplyItem.validateTotalAmount(_totalDoseController.text);
   String? get _concentrationError =>
       SupplyItem.validateConcentration(_concentrationController.text);
+  String? get _moleculeError => SupplyItem.validateMolecule(_molecule);
+  String? get _administrationRouteError =>
+      SupplyItem.validateAdministrationRoute(_administrationRoute);
+  String? get _esterError {
+    final validator =
+        SupplyItem.esterValidator(_molecule, _administrationRoute);
+    return validator(_ester);
+  }
 
   bool get _isFormValid =>
       _nameError == null &&
       _totalDoseError == null &&
-      _concentrationError == null;
+      _concentrationError == null &&
+      _moleculeError == null &&
+      _administrationRouteError == null &&
+      _esterError == null;
 
-  @override
-  void initState() {
-    super.initState();
-    _totalDoseController = TextEditingController();
-    _nameController = TextEditingController();
-    _concentrationController = TextEditingController();
+  bool get _useEsterField =>
+      _molecule == KnownMolecules.estradiol &&
+      _administrationRoute == AdministrationRoute.injection;
+
+  void _onMoleculeChanged(Molecule? molecule) {
+    if (molecule != null) {
+      setState(() {
+        _molecule = molecule;
+
+        if (!_useEsterField) {
+          _ester = null;
+        }
+      });
+    }
   }
 
-  @override
-  void dispose() {
-    _totalDoseController.dispose();
-    _nameController.dispose();
-    _concentrationController.dispose();
-    super.dispose();
+  void _onAdministrationRouteChanged(AdministrationRoute? administrationRoute) {
+    if (administrationRoute != null) {
+      setState(() {
+        _administrationRoute = administrationRoute;
+
+        if (!_useEsterField) {
+          _ester = null;
+        }
+      });
+    }
+  }
+
+  void _onEsterChanged(Ester? ester) {
+    if (ester != null) {
+      setState(() {
+        _ester = ester;
+      });
+    }
   }
 
   void _refresh() {
@@ -55,15 +93,36 @@ class _NewItemPageState extends State<NewItemPage> {
     final name = _nameController.text;
     final supplyItemProvider =
         Provider.of<SupplyItemProvider>(context, listen: false);
+
     final item = SupplyItem(
-        name: name,
-        totalDose: totalDose,
-        concentration: concentration,
-        molecule: KnownMolecules.estradiol,
-        administrationRoute: AdministrationRoute.injection,
-        ester: Ester.enanthate);
+      name: name,
+      totalDose: totalDose,
+      concentration: concentration,
+      molecule: _molecule!,
+      administrationRoute: _administrationRoute!,
+      ester: _ester,
+    );
     supplyItemProvider.add(item);
+
     Navigator.pop(context);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _preferencesService =
+        Provider.of<PreferencesService>(context, listen: false);
+    _totalDoseController = TextEditingController();
+    _nameController = TextEditingController();
+    _concentrationController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _totalDoseController.dispose();
+    _nameController.dispose();
+    _concentrationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -80,12 +139,33 @@ class _NewItemPageState extends State<NewItemPage> {
           onChanged: _refresh,
           inputType: TextInputType.text,
         ),
+        FormSpacer(),
+        FormDropdownField<Molecule>(
+          value: _molecule,
+          items: _preferencesService.moleculeDropdownItems,
+          onChanged: _onMoleculeChanged,
+          label: 'Molecule',
+        ),
+        FormDropdownField<AdministrationRoute>(
+          value: _administrationRoute,
+          items: AdministrationRoute.menuItems,
+          onChanged: _onAdministrationRouteChanged,
+          label: 'Administration route',
+        ),
+        if (_useEsterField)
+          FormDropdownField<Ester>(
+            value: _ester,
+            items: EsterDropdown.menuItems,
+            onChanged: _onEsterChanged,
+            label: 'Ester',
+          ),
+        FormSpacer(),
         FormTextField(
           controller: _totalDoseController,
           label: 'Total amount',
           onChanged: _refresh,
           inputType: TextInputType.numberWithOptions(decimal: true),
-          suffixText: 'mg',
+          suffixText: _molecule?.unit,
           regexFormatter: r'[0-9.,]',
         ),
         FormTextField(
@@ -93,7 +173,7 @@ class _NewItemPageState extends State<NewItemPage> {
           label: 'Concentration',
           onChanged: _refresh,
           inputType: TextInputType.numberWithOptions(decimal: true),
-          suffixText: 'mg/ml',
+          suffixText: '${_molecule?.unit}/${_administrationRoute?.unit}',
           regexFormatter: r'[0-9.,]',
         ),
       ],
