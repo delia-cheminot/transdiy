f212bdee6fa4ed383ee717c5354b966b35d605ae
Seremptos <thomasf60120@gmail.com>
2026-03-16 02:22:51 +0100

    chore(dialogs): Made dialogs more modulable

--- lib/ui/widgets/dialogs.dart
+++ lib/ui/widgets/dialogs.dart
@@ -1,27 +1,46 @@
 import 'package:flutter/material.dart';
 
 class Dialogs {
-  static Future<bool?> confirmDelete(BuildContext context) {
+
+  static const cantBeUndone = Text("This action can't be undone.");
+  static const delete = Text("Delete");
+  static const cancel = Text("Cancel");
+
+  static Future<bool?> confirmDialog(BuildContext context, Text title, Text content, Text no, Text yes) {
     return showDialog<bool>(
       context: context,
       barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Delete this element?'),
-          content: Text(
-              'This action cannot be undone.'),
+          title: title,
+          content: content,
           actions: <Widget>[
             TextButton(
               onPressed: () => Navigator.of(context).pop(false),
-              child: Text('Cancel'),
+              child: no,
             ),
             TextButton(
               onPressed: () => Navigator.of(context).pop(true),
-              child: Text('Delete'),
+              child: yes,
             ),
           ],
         );
       },
     );
+  }
+
+  static Future<bool?> confirmDeleteIntake(BuildContext context) {
+    return confirmDialog(context,
+        const Text("Delete this intake?"),
+        cantBeUndone,
+        cancel, delete);
+  }
+
+
+  static Future<bool?> confirmDelete(BuildContext context) {
+    return confirmDialog(context,
+        const Text("Delete this element?"),
+        cantBeUndone,
+        cancel, delete);
   }
 }
