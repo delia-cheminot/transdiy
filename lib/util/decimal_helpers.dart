04c76278171a511972411350333002222eb01ccd
délia <delia@cheminot.net>
2026-03-23 19:53:15 +0100

    chore: simplify parseDecimal

--- lib/util/decimal_helpers.dart
+++ lib/util/decimal_helpers.dart
@@ -1,12 +1,11 @@
 import 'package:decimal/decimal.dart';
 
-Decimal parseDecimal(String text) {
-  final sanitizedText = text.trim().replaceAll(',', '.');
-  return Decimal.parse(sanitizedText);
-}
+Decimal parseDecimal(String text) =>
+    Decimal.parse(text.trim().replaceAll(',', '.'));
 
 Decimal? parseOptionalDecimal(String decimalString) {
   final decimal = decimalString.trim();
   if (decimal.isEmpty) return null;
+
   return parseDecimal(decimal);
 }
