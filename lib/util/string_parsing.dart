e8ec570e363914708d38bd8b136b51d4c2c58a85
délia <delia@cheminot.net>
2026-04-08 00:58:27 +0200

    fix: add try/catch to parsers

--- lib/util/string_parsing.dart
+++ lib/util/string_parsing.dart
@@ -1,33 +1,44 @@
 import 'package:decimal/decimal.dart';
 
-extension EmptyString on String? {
-  String get _trimmedString => this?.trim().replaceAll(',', '.') ?? '';
+String _sanitize(String? input) => input?.trim().replaceAll(',', '.') ?? '';
 
-  bool get isEmpty => _trimmedString.isEmpty;
+extension EmptyString on String? {
+  bool get isEmpty => _sanitize(this).isEmpty;
 }
 
 // TODO this can be done on String only as text controllers are never null
 extension DecimalParsing on String? {
   bool get isDecimal => toDecimalOrNull != null;
 
-  Decimal get toDecimalOrZero =>
-      Decimal.tryParse(_trimmedString) ?? Decimal.zero;
+  Decimal get toDecimalOrZero {
+    try {
+      return Decimal.tryParse(_sanitize(this)) ?? Decimal.zero;
+    } catch (_) {
+      return Decimal.zero;
+    }
+  }
 
-  Decimal? get toDecimalOrNull => Decimal.tryParse(_trimmedString);
+  Decimal? get toDecimalOrNull {
+    try {
+      return Decimal.tryParse(_sanitize(this));
+    } catch (_) {
+      return null;
+    }
+  }
 
   // TODO test
-  Decimal get toDecimal => Decimal.parse(_trimmedString);
+  Decimal get toDecimal => Decimal.parse(_sanitize(this));
 }
 
 extension DateTimeParsing on String? {
   // TODO test
-  DateTime? get toDateTimeOrNull => DateTime.tryParse(_trimmedString);
+  DateTime? get toDateTimeOrNull => DateTime.tryParse(_sanitize(this));
 
   DateTime get toDateTime => DateTime.parse(this!);
 }
 
 extension IntParsing on String? {
-  int get intOrZero => int.tryParse(_trimmedString) ?? 0;
+  int get intOrZero => int.tryParse(_sanitize(this)) ?? 0;
 
-  int get toInt => int.parse(_trimmedString);
+  int get toInt => int.parse(_sanitize(this));
 }
