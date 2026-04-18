44d45017fea9aeb21cb6c8ac5befd9b77df0a20c
Délia Cheminot <delia.cheminot@gmail.com>
2026-04-18 12:33:59 +0200

    chore: regenerate mocks

--- test/controllers/notification_scheduler_test.mocks.dart
+++ test/controllers/notification_scheduler_test.mocks.dart
@@ -430,13 +430,6 @@ class MockPreferencesService extends _i1.Mock
       ) as List<_i12.DropdownMenuItem<_i11.Molecule>>);
 
   @override
-  bool get shouldShowScheduleDialog => (super.noSuchMethod(
-        Invocation.getter(#shouldShowScheduleDialog),
-        returnValue: false,
-        returnValueForMissingStub: false,
-      ) as bool);
-
-  @override
   bool get hasListeners => (super.noSuchMethod(
         Invocation.getter(#hasListeners),
         returnValue: false,
@@ -481,16 +474,6 @@ class MockPreferencesService extends _i1.Mock
         Invocation.method(
           #removeCustomMolecule,
           [name],
-        ),
-        returnValue: _i5.Future<void>.value(),
-        returnValueForMissingStub: _i5.Future<void>.value(),
-      ) as _i5.Future<void>);
-
-  @override
-  _i5.Future<void> setShowScheduleDialog(bool? value) => (super.noSuchMethod(
-        Invocation.method(
-          #setShowScheduleDialog,
-          [value],
         ),
         returnValue: _i5.Future<void>.value(),
         returnValueForMissingStub: _i5.Future<void>.value(),
